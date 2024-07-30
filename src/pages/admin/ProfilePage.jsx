cb630c43d3b64ac651e6855e717e2e5fe6903520
aashiashfak <aashiashfak@gmail.com>
2024-07-30 18:58:49 +0530

    added admin profile page

---
+++ src/pages/admin/ProfilePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProfilePage = () => {
+  return (
+    <div>
+      Entered in Admin Profile Page
+    </div>
+  )
+}
+
+export default ProfilePage
+Entered in Admin Profile Page
